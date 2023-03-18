10db75fa3fe01c1c2cad42766a7af82f9224d929
Rahul Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2023-03-18 18:03:43 +0530

    add hook

--- components/Authentication/SignIn.tsx
+++ components/Authentication/SignIn.tsx
@@ -1,8 +1,6 @@
 import { Fragment, useRef, useState } from "react";
 import { MdCancel } from 'react-icons/md';
-
 import { Dialog, Transition } from "@headlessui/react";
-import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import useSignInStore from "../../store/SignIn_SignOut";
 
 export default function SignIn(): JSX.Element {
@@ -11,6 +9,10 @@ export default function SignIn(): JSX.Element {
     const [password, setPassword] = useState("")
 
     const cancelButtonRef = useRef(null);
+    const handleSignIn = async()=>{
+        console.log(email);
+        console.log(password);
+    }
 
     return (
         <Transition.Root show={state.open} as={Fragment}>
@@ -43,16 +45,16 @@ export default function SignIn(): JSX.Element {
                             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         >
-                            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-transparent p-11 text-left transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                <div className="p-2 flex">
-                                    <div>
+                            <Dialog.Panel className="relative transform  rounded-lg bg-transparent p-10 text-left transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                <div className="p-2 flex " >
+                                    <div >
                                         {/* for email and password entry */}
                                         {/* <!-- component --> */}
                                         {/* <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12"> */}
-                                            <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+                                            <div className="relative py-3 sm:max-w-xl sm:mx-auto  ">
                                                 
                                                 <div
-                                                    className="absolute inset-0 bg-gradient-to-r from-[#ec008c] to-[#fc6767] shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+                                                    className="absolute inset-0 bg-gradient-to-r from-[#ec008c] to-[#fc6767] shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl ">
                                                 </div>
                                                 
                                                 <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
@@ -66,22 +68,31 @@ export default function SignIn(): JSX.Element {
                                                         </p>
                                                         
                                                     </div>
-                                                        <div >
-                                                            <h1 className="text-2xl font-semibold">Sign In</h1>
+                                                        <div className="w-80" >
+                                                            <h1 className="text-2xl font-semibold ">Sign In</h1>
                                                         </div>
                                                         <div className="divide-y divide-gray-200">
                                                             <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                                                                 <div className="relative ">
-                                                                    <input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
+                                                                    <input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address"
+                                                                    value={email}
+                                                                    onChange={(e) => setEmail(e.target.value)}
+                                                                    />
                                                                     <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm
-                                                                    ">Email Address</label>
+                                                                    "
+                                                                    >Email Address</label>
                                                                 </div>
                                                                 <div className="relative top-3">
-                                                                    <input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+                                                                    <input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" 
+                                                                    value={password}
+                                                                    onChange={(e) => setPassword(e.target.value)}
+                                                                    />
                                                                     <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                                                                 </div>
                                                                 <div className="relative top-6">
-                                                                <button className="bg-gradient-to-r from-[#ec008c] to-[#fc6767] text-white rounded-md px-2 py-1 ">Submit</button>
+                                                                <button className="bg-gradient-to-r from-[#ec008c] to-[#fc6767] text-white rounded-md px-2 py-1 "
+                                                                onClick={handleSignIn}
+                                                                >Submit</button>
                                                                 </div>
                                                                 
                                                             </div>
--- components/Authentication/SignUp.tsx
+++ components/Authentication/SignUp.tsx
@@ -1,7 +1,7 @@
 import { Fragment, useRef, useState } from "react";
+import { MdCancel } from 'react-icons/md';
 import { Dialog, Transition } from "@headlessui/react";
-import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
-import  { useSignUpStore } from "../../store/SignIn_SignOut";
+import {useSignUpStore} from "../../store/SignIn_SignOut";
 
 export default function SignUp(): JSX.Element {
     const state = useSignUpStore()
@@ -9,6 +9,10 @@ export default function SignUp(): JSX.Element {
     const [password, setPassword] = useState("")
 
     const cancelButtonRef = useRef(null);
+    const handleSignIn = async () => {
+        console.log(email);
+        console.log(password);
+    }
 
     return (
         <Transition.Root show={state.open} as={Fragment}>
@@ -41,49 +45,69 @@ export default function SignUp(): JSX.Element {
                             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         >
-                            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                    <div className="sm:flex sm:items-start">
-                                        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                                            <ExclamationTriangleIcon
-                                                className="h-6 w-6 text-red-600"
-                                                aria-hidden="true"
-                                            />
-                                        </div>
-                                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                            <Dialog.Title
-                                                as="h3"
-                                                className="text-base font-semibold leading-6 text-gray-900"
-                                            >
-                                                Deactivate account
-                                            </Dialog.Title>
-                                            <div className="mt-2">
-                                                <p className="text-sm text-gray-500">
-                                                    Are you sure you want to deactivate your account? All
-                                                    of your data will be permanently removed. This action
-                                                    cannot be undone.
-                                                </p>
+                            <Dialog.Panel className="relative transform  rounded-lg bg-transparent p-10 text-left transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                <div className="p-2 flex " >
+                                    <div >
+                                        {/* for email and password entry */}
+                                        {/* <!-- component --> */}
+                                        {/* <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12"> */}
+                                        <div className="relative py-3 sm:max-w-xl sm:mx-auto  ">
+
+                                            <div
+                                                className="absolute inset-0 bg-gradient-to-r from-[#ec008c] to-[#fc6767] shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl ">
+                                            </div>
+
+                                            <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+                                                <div className="max-w-md mx-auto">
+                                                    <div className="absolute right-6 top-5">
+                                                        <p
+                                                            onClick={() => state.setOpen(state.open)}
+                                                            ref={cancelButtonRef}
+                                                        >
+                                                            <MdCancel className="invert" fontSize="2rem" color="white" />
+                                                        </p>
+
+                                                    </div>
+                                                    <div className="w-80" >
+                                                        <h1 className="text-2xl font-semibold ">Sign In</h1>
+                                                    </div>
+                                                    <div className="divide-y divide-gray-200">
+                                                        <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                                                            <div className="relative ">
+                                                                <input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address"
+                                                                    value={email}
+                                                                    onChange={(e) => setEmail(e.target.value)}
+                                                                />
+                                                                <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm
+                                                                    "
+                                                                >Email Address</label>
+                                                            </div>
+                                                            <div className="relative top-3">
+                                                                <input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password"
+                                                                    value={password}
+                                                                    onChange={(e) => setPassword(e.target.value)}
+                                                                />
+                                                                <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                                                            </div>
+                                                            <div className="relative top-6">
+                                                                <button className="bg-gradient-to-r from-[#ec008c] to-[#fc6767] text-white rounded-md px-2 py-1 "
+                                                                    onClick={handleSignIn}
+                                                                >Submit</button>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
+                                        {/* </div> */}
+
+                                    </div>
+                                    <div>
+                                        {/* for svg */}
                                     </div>
                                 </div>
-                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                    <button
-                                        type="button"
-                                        className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                                        onClick={() => state.setOpen(state.open)}
-                                    >
-                                        Deactivate
-                                    </button>
-                                    <button
-                                        type="button"
-                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                                        onClick={() => state.setOpen(state.open)}
-                                        ref={cancelButtonRef}
-                                    >
-                                        Cancel
-                                    </button>
-                                </div>
+
                             </Dialog.Panel>
                         </Transition.Child>
                     </div>
