e64c4d3a7fbe989b7be408d249e429b0d1c17d40
abhishekrathi16 <rathiabhishek53@gmail.com>
2023-03-18 22:46:05 +0530

    add footer and home page

--- components/HomePageComponent/HomePage.tsx
+++ components/HomePageComponent/HomePage.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import DisplayTemplates from './DisplayTemplates'
-import GetStarted from './GetStarted'
-import GuideToMakeBetterResume from './GuideToMakeBetterResume'
+import React from "react";
+import DisplayTemplates from "./DisplayTemplates";
+import GetStarted from "./GetStarted";
+import GuideToMakeBetterResume from "./GuideToMakeBetterResume";
 
-const HomePage = ():JSX.Element => {
+const HomePage = (): JSX.Element => {
   return (
     <>
-      <GetStarted />
-      <DisplayTemplates />
-      <GuideToMakeBetterResume /> 
+      <div className="h-[100vh]">
+        <GetStarted />
+        <DisplayTemplates />
+        <GuideToMakeBetterResume />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
