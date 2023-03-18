d7068e8e4e04fe3d5e052b1ceb3e965b9c8032d1
abhishekrathi16 <rathiabhishek53@gmail.com>
2023-03-18 10:34:52 +0530

    add flowbite

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,11 @@
 import { ScriptProps } from "next/script";
+import NavBar from "./Navbar";
 
 const Layout: React.FC<ScriptProps> = ({ children }) => {
   return (
     <>
     <div>Layout</div>
+    <NavBar/>
     <div>{children}</div>
     </>
   );
