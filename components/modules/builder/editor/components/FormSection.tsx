6eca636daa8bcdd431e31a5c03d81cb89b06697c
abhishekrathi16 <rathiabhishek53@gmail.com>
2023-03-29 11:18:51 +0530

    resolve errors and fix sidebar

--- components/modules/builder/editor/components/FormSection.tsx
+++ components/modules/builder/editor/components/FormSection.tsx
@@ -4,6 +4,7 @@ import EducationLayout from "../modules/education/EducationLayout";
 import ExperienceLayout from "../modules/experience/ExperienceLayout";
 import SkillsLayout from "../modules/skills/SkillsLayout";
 import AwardsLayout from "../modules/awards/AwardsLayout";
+import ProjectsLayout from "../modules/projects/ProjectsLayout";
 
 interface Id {
   id: number;
@@ -20,6 +21,8 @@ const switchLayouts = (id: number): JSX.Element => {
     case 4:
       return <ExperienceLayout />;
     case 5:
+      return <ProjectsLayout />;
+    case 6:
       return <AwardsLayout />;
     default:
       return <></>;
